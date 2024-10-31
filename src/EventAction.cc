2347d879fd232748fff21edb1f4ae6797175ffb8
arnon <arnon3339@gmail.com>
2024-10-31 21:02:03 +0700

    fixed all thickness

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -46,55 +46,41 @@ namespace kcmh
     for (size_t i = 0; i < DTC->GetSize(); i++)
     {
       analysisManager->FillH1(0, (*DTC)[i]->GetLayerID());
-      if ((*DTC)[i]->GetLayerID() == 0)
-        analysisManager->FillH2(0, (*DTC)[i]->GetPixels()[0], 
-          (*DTC)[i]->GetPixels()[1]);
-
-      analysisManager->FillNtupleIColumn(0, event->GetEventID());
-      analysisManager->FillNtupleIColumn(1, (*DTC)[i]->GetPixels()[0]);
-      analysisManager->FillNtupleIColumn(2, (*DTC)[i]->GetPixels()[1]);
-      analysisManager->FillNtupleIColumn(3, (*DTC)[i]->GetLayerID());
-      analysisManager->FillNtupleDColumn(4, (*DTC)[i]->GetEdep());
       if (((*DTC)[i]->GetLayerID() != 0) && ((*DTC)[i]->GetLayerID() != 1) &&
-        ((*DTC)[i-1]->GetLayerID() == ((*DTC)[i]->GetLayerID() -1)))
+        ((*DTC)[i-1]->GetLayerID() == ((*DTC)[i]->GetLayerID() - 1)))
       {
         G4double refVector[] = {0., 0., dtcDistance};
         G4double currentXY[] = {
-          (*DTC)[i]->GetPixels()[0]*pixelSizeX,
-          (*DTC)[i]->GetPixels()[1]*pixelSizeY
-          };
+          (*DTC)[i]->GetPixels()[0] * pixelSizeX,
+          (*DTC)[i]->GetPixels()[1] * pixelSizeY
+        };
         G4double prevXY[] = {
-          (*DTC)[i-1]->GetPixels()[0]*pixelSizeX,
-          (*DTC)[i-1]->GetPixels()[1]*pixelSizeY
-          };
+          (*DTC)[i-1]->GetPixels()[0] * pixelSizeX,
+          (*DTC)[i-1]->GetPixels()[1] * pixelSizeY
+        };
 
         G4double theVector[] = {
           currentXY[0] - prevXY[0],
           currentXY[1] - prevXY[1],
           dtcDistance
-          };
+        };
 
-        G4double dotProduct = 
-          refVector[0]*theVector[0] + refVector[1]*theVector[1] +
-          refVector[2]*theVector[2];
-        
-        G4double normTheVector = std::sqrt(
-          std::pow(theVector[0], 2) + std::pow(theVector[1], 2) +
-          std::pow(theVector[2], 2)
-        );
-        G4double normRefVector = std::sqrt(
-          std::pow(refVector[0], 2) + std::pow(refVector[1], 2) +
-          std::pow(refVector[2], 2)
-        );
+        G4double dotProduct =
+          refVector[0] * theVector[0] + refVector[1] * theVector[1] +
+          refVector[2] * theVector[2];
+
+        G4double normTheVector = std::hypot(theVector[0], theVector[1], theVector[2]);
+        G4double normRefVector = std::hypot(refVector[0], refVector[1], refVector[2]);
 
         G4double angle = 0.;
-        G4double productTwoNorms = normRefVector*normTheVector;
-        if (dotProduct > productTwoNorms) angle = 0.;
-        else angle = std::acos(dotProduct/productTwoNorms);
+        G4double productTwoNorms = normRefVector * normTheVector;
 
-        analysisManager->FillNtupleDColumn(5, 
-          dtcDistance*(1/std::cos(angle))
-        );
+        if (dotProduct > productTwoNorms) angle = 0.;
+        else angle = std::acos(std::clamp(dotProduct / productTwoNorms, -1.0, 1.0));
+
+        if (std::abs(std::cos(angle)) > 1e-6) 
+          analysisManager->FillNtupleDColumn(5, dtcDistance * (1 / std::cos(angle)));
+        else analysisManager->FillNtupleDColumn(5, 0.);
       }
       else
         analysisManager->FillNtupleDColumn(5, 0.);
